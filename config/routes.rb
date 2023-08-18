da7f30537055053186f88d940f98038e4d0aed80
John Rose <johnedmundrose@gmail.com>
2023-08-17 22:08:02 -0400

    Default route to users; formatting; add humor

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#hello"
+  root "users#index"
 end
