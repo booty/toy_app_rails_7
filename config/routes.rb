1a6132e0bc4ca6834dcb09c382f32c2c08c061e9
John Rose <johnedmundrose@gmail.com>
2023-08-17 22:10:46 -0400

    micropost scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
 
   # Defines the root path route ("/")
